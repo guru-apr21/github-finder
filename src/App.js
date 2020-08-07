56d8f2e2521414e1433e91e015ce6c0ccc24cc69
Guru Prakash Ram <69163711+guru-apr21@users.noreply.github.com>
2020-08-07 08:24:03 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello from Mosh</h1>
+      <p>Let's put our hope in Brad and in his teaching techniques.</p>
     </div>
   );
 }
